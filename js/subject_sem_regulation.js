6c2842355130e021228ba9a9eb55f96cb6aae389
Vidya-9 <vidyagolta509@gmail.com>
2021-06-12 12:35:53 +0530

    working files for updating student sem marks

--- js/subject_sem_regulation.js
+++ js/subject_sem_regulation.js
@@ -1,3 +1,23 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+        
+    apiKey: "AIzaSyDuu4FIsR0yRIkOrtu6T59lqfPbN7SG53M",
+    authDomain: "sms-demo-9ec85.firebaseapp.com",
+    databaseURL: "https://sms-demo-9ec85-default-rtdb.firebaseio.com",
+    projectId: "sms-demo-9ec85",
+    storageBucket: "sms-demo-9ec85.appspot.com",
+    messagingSenderId: "401316316472",
+    appId: "1:401316316472:web:2929d9673d92fbfbb66dcd"
+};
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+
+var regulation = "R15"; 
+var branch = "CSE";
+var semester = "Semester-1";
 
 //src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
@@ -61,34 +81,34 @@
              
 
                
-                var regulation =  document.getElementById("regulation");
+                const regulationRef = document.getElementById("regulation");
                 
                  
-                  if(regulation.value == "")
+                  if(regulationRef.value == "")
                     {
                        alert("Please Enter Regulation");
-                      regulation.focus();
+                      regulationRef.focus();
                        return false;
                     }
                 
                
                 //branch
-                var branch=document.getElementById("branch");
-                if(branch.value=="branch")
+                const branchRef = document.getElementById("branch");
+                if(branchRef.value=="branch")
                 {
                    alert("Please Select Branch!!");
-                   branch.focus();
+                   branchRef.focus();
                    return false;
                 } 
                 
 
-                //semister
-                var semister=document.getElementById("semister");
-                if(semister.value=="sem")
+                //semester
+                const semesterRef = document.getElementById("semester");
+                if(semesterRef.value=="sem")
                 {
                   
-                alert("Please Select semister!!");
-                semister.focus();
+                alert("Please Select semester!!");
+                semesterRef.focus();
                 return false;
                 }
 
@@ -112,10 +132,15 @@
                 return false;
                 }*/
             
-                else
-                    {
-                        alert("Data Saved Successfully!!")
-                    }
+                // else
+                //     {
+                //         alert("Data Saved Successfully!!")
+                //     }
+
+                regulation = regulationRef.value.toUpperCase();
+                branch = branchRef.value;
+                semester = semesterRef.value;
+                return true;
                 }
                     
                     
@@ -155,4 +180,141 @@
                 }
             }*/
 
-           
+setTimeout(() => {
+    document.querySelector("#newrow").click();
+    document.querySelector("#newlabrow").click();  
+
+}, 100);
+
+
+const saveBtn = document.querySelector("#export1");
+
+saveBtn.addEventListener("click", (event) => {
+
+    if(check()) {
+
+        event.preventDefault();
+        //gets table
+        var oTable = document.getElementById('subject');
+
+        //gets rows of table
+        var rowLength = oTable.rows.length;
+
+        var subjects = [];
+
+        //loops through rows    
+        for (i = 1; i < rowLength; i++){
+
+            //gets cells of current row
+            var oCells = oTable.rows.item(i).cells;
+
+            //gets amount of cells of current row
+            var cellLength = oCells.length;
+
+            //loops through each cell in current row
+            var map = {
+                subject_code: "",
+                subject_name: "",
+                subject_short_name: "" 
+            };
+            
+            for(var j = 1; j < cellLength; j++){
+                /* get your cell info here */
+                /* var cellVal = oCells.item(j).innerHTML; */
+                if (j == 1) {
+
+                    map.subject_code = oTable.rows[i].cells[j].getElementsByTagName("input")[0].value.toUpperCase();
+                }
+                else if ( j == 2) {
+
+                    let temp = oTable.rows[i].cells[j].getElementsByTagName("input")[0].value;
+                    map.subject_name = capitalize(temp);
+                }
+                
+                else {
+
+                    map.subject_short_name = oTable.rows[i].cells[j].getElementsByTagName("input")[0].value.toUpperCase();
+                }
+            }
+            // console.log("Success");
+            subjects.push(map);
+        }
+        console.log(subjects);
+
+        /****** For labs **********/
+        oTable = document.getElementById('lab');
+
+        //gets rows of table
+        rowLength = oTable.rows.length;
+
+        var labs = [];
+
+        //loops through rows    
+        for (i = 1; i < rowLength; i++){
+
+            //gets cells of current row
+            var oCells = oTable.rows.item(i).cells;
+
+            //gets amount of cells of current row
+            var cellLength = oCells.length;
+
+            //loops through each cell in current row
+            var map = {
+                lab_code: "",
+                lab_name: "",
+                lab_short_name: "" 
+            };
+            
+            for(var j = 1; j < cellLength; j++){
+                /* get your cell info here */
+                /* var cellVal = oCells.item(j).innerHTML; */
+                if (j == 1) {
+
+                    map.lab_code = oTable.rows[i].cells[j].getElementsByTagName("input")[0].value.toUpperCase();
+                }
+                else if ( j == 2) {
+
+                    let temp = oTable.rows[i].cells[j].getElementsByTagName("input")[0].value;
+                    map.lab_name = capitalize(temp);
+                }
+                
+                else {
+
+                    map.lab_short_name = oTable.rows[i].cells[j].getElementsByTagName("input")[0].value.toUpperCase();
+                }
+            }
+            // console.log("Success");
+            labs.push(map);
+        }
+        console.log(labs);
+
+        database.ref("Subjects/" + regulation + "/" + branch + "/" + semester).set(subjects);
+        database.ref("Labs/" + regulation + "/" + branch + "/" + semester).set(labs);
+        
+        alert("Subjects and Regulation saved successfully!");
+    }
+});
+
+
+function capitalize(str) {
+
+    var temp = str.split(" ");
+    var res = "";
+    for (let i = 0; i < temp.length; i ++) {
+
+        res += temp[i].substring(0, 1).toUpperCase() + temp[i].substring(1) + " ";
+    }
+    return res;
+}
+
+// Database fetch subjects
+
+// database.ref().child("Subjects").child("R15").child("CSE").child("Semester-1").get().then(  (snapshot) => {
+  
+//     if (snapshot.exists()) {
+//         console.log(snapshot.val());
+//     }
+// })
+// .catch((e) => {
+//       console.log(e);
+//   });
